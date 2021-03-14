2a474a7eea7a5356d83afc3e9ea508b3ac8dc5bc
Aidan Molloy <aidan@molloy.ie>
2021-03-14 02:17:23 +0000

    I/O problems

---
+++ Kattis/carrots.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int INF = 1e9;
+const ll LLINF = 4e18;
+const double EPS = 1e-9;
+
+int main() {
+  int n, p = 0;
+  scanf("%d %d", &n,&p);
+  printf("%d", p);
+  return 0;
+}
---
+++ UVa/13025.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int INF = 1e9;
+const ll LLINF = 4e18;
+const double EPS = 1e-9;
+
+int main() {
+  printf("May 29, 2013 Wednesday\n");
+  return 0;
+}
