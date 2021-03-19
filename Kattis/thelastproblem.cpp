d93fc5b7638377a473adca593e5436328db7a37d
Aidan Molloy <aidan@molloy.ie>
2021-03-19 01:19:28 +0000

    Kattis I/O problems

---
+++ Kattis/thelastproblem.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int INF = 1e9;
+const ll LLINF = 4e18;
+const double EPS = 1e-9;
+
+int main() {
+  string s;
+  getline(cin, s);
+  cout<<"Thank you, " << s << ", and farewell!";
+  return 0;
+}
